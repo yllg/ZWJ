376d5a561f7cf4c82ce1701e5f1022246044b26a
yllg <1565550208@qq.com>
2017-08-05 23:22:23 +0800

    重力感应之后再做

--- scripts/index.js
+++ scripts/index.js
@@ -68,7 +68,7 @@ window.addEventListener('deviceorientation', function(event) {
 	//获得手机旋转的gamma，如果手机转则box也旋转，同时flag为false禁止手指拖动了
 	//也就是手机Y轴转超过60度的时候，手碰它是没有用的。
 	var gamma = event.gamma;
-	if (Math.abs(gamma) > 20) {
+	if (Math.abs(gamma) > 60) {
 		flag = false;
 		var gammaY = x +gamma;
 		box.style.transform = 'rotateY(' + gammaY + 'deg)';
