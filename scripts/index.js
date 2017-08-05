92baf0db944768bc7864a08c0bf243775855cd7c
yllg <1565550208@qq.com>
2017-08-05 22:17:50 +0800

    优化手机倾斜角度的灵敏度，手指拨动画面的方向

--- scripts/index.js
+++ scripts/index.js
@@ -40,14 +40,17 @@ $('#box').on('touchstart', function(event) {
 	//手指碰屏幕自动旋转停止
 	clearInterval(autoRoteId);
 	var touch = event.targetTouches[0];
-	startX = touch.pageX - x;
+	// startX = touch.pageX - x;
+	startX = touch.pageX;
 })
 $('#box').on('touchmove', function(event) {
 	if (flag) {
 		event.preventDefault();
 		var touch = event.targetTouches[0];
 		endX = touch.pageX;
-		x = endX - startX;
+		
+		// x = endX - startX;
+		x -= endX - startX ;
 		box.style.transform = 'rotateY(' + x + 'deg)';
 	} else {
 		return false;
@@ -65,7 +68,7 @@ window.addEventListener('deviceorientation', function(event) {
 	//获得手机旋转的gamma，如果手机转则box也旋转，同时flag为false禁止手指拖动了
 	//也就是手机转的时候，手碰它是没有用的。
 	var gamma = event.gamma;
-	if (Math.abs(gamma) > 1) {
+	if (Math.abs(gamma) > 20) {
 		flag = false;
 		box.style.transform = 'rotateY(' + gamma * 3 + 'deg)';
 	} else {
